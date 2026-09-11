05ae719ba8987f9b07372b7432cb9f2ac61cc250
Pedro Gaudencio <gsilvapedro370@gmail.com>
2026-09-11 15:13:43 -0300

    Primeiro commit do projeto

---
+++ frontend/script.js
@@ -0,0 +1,160 @@
+let botaoProdutos = document.getElementById("carregarProdutos");
+let areaProdutos = document.getElementById("produtos");
+
+
+function carregarProdutos() {
+
+    fetch("http://127.0.0.1:8000/produtos")
+        .then(resposta => resposta.json())
+        .then(listaProdutos => {
+
+            console.log("Produtos recebidos:", listaProdutos);
+
+            areaProdutos.innerHTML = "";
+
+            listaProdutos.forEach(produto => {
+
+                let elemento = document.createElement("div");
+
+                elemento.classList.add("produto");
+
+                elemento.innerHTML = `
+                    <h3>${produto.nome}</h3>
+                    <p><strong>ID:</strong> ${produto.id}</p>
+                    <p><strong>Preço:</strong> R$ ${produto.preco}</p>
+                    <p><strong>Estoque:</strong> ${produto.estoque}</p>
+                `;
+
+                areaProdutos.appendChild(elemento);
+            });
+
+        })
+        .catch(erro => {
+            console.log("Erro:", erro);
+        });
+}
+
+
+botaoProdutos.addEventListener("click", function() {
+    carregarProdutos();
+});
+
+
+// CADASTRAR PRODUTO
+
+let botaoCadastrar = document.getElementById("cadastrarProduto");
+
+botaoCadastrar.addEventListener("click", function() {
+
+    let nome = document.getElementById("nomeProduto").value;
+    let preco = Number(document.getElementById("precoProduto").value);
+    let estoque = Number(document.getElementById("estoqueProduto").value);
+
+    let token = localStorage.getItem("token");
+
+    fetch("http://127.0.0.1:8000/produtos", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        },
+
+        body: JSON.stringify({
+            nome: nome,
+            preco: preco,
+            estoque: estoque
+        })
+
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+
+        console.log("Produto cadastrado:", dados);
+
+        carregarProdutos();
+
+    })
+    .catch(erro => {
+        console.log("Erro:", erro);
+    });
+
+});
+
+
+// EDITAR PRODUTO
+
+let botaoEditar = document.getElementById("editarProduto");
+
+botaoEditar.addEventListener("click", function() {
+
+    let id = document.getElementById("idProdutoEditar").value;
+    let nome = document.getElementById("nomeProdutoEditar").value;
+    let preco = Number(document.getElementById("precoProdutoEditar").value);
+    let estoque = Number(document.getElementById("estoqueProdutoEditar").value);
+
+    let token = localStorage.getItem("token");
+
+    fetch("http://127.0.0.1:8000/produtos/" + id, {
+
+        method: "PUT",
+
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token
+        },
+
+        body: JSON.stringify({
+            nome: nome,
+            preco: preco,
+            estoque: estoque
+        })
+
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+
+        console.log("Produto atualizado:", dados);
+
+        carregarProdutos();
+
+    })
+    .catch(erro => {
+        console.log("Erro:", erro);
+    });
+
+});
+
+// EXCLUIR PRODUTO
+
+let botaoExcluir = document.getElementById("excluirProduto");
+
+botaoExcluir.addEventListener("click", function() {
+
+    let id = document.getElementById("idProdutoExcluir").value;
+
+    let token = localStorage.getItem("token");
+
+    fetch("http://127.0.0.1:8000/produtos/" + id, {
+
+        method: "DELETE",
+
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+
+        console.log("Produto excluído:", dados);
+
+        carregarProdutos();
+
+    })
+    .catch(erro => {
+        console.log("Erro:", erro);
+    });
+
+});
---
+++ main.py
@@ -0,0 +1,118 @@
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+from typing import Annotated
+from database import criar_tabela, criar_tabela_usuarios, listar_produtos, buscar_produtos_nome, adicionar_produto, buscar_produto_id, atualizar_produto_db, excluir_produto_db, buscar_produtos_estoque, buscar_usuario_username, verificar_senha
+import jwt
+from jwt.exceptions import InvalidTokenError
+from fastapi.security import HTTPBearer
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI(title='API')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:63342"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+criar_tabela()
+criar_tabela_usuarios()
+security = HTTPBearer()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+class ProdutoCriacao(BaseModel):
+    nome: Annotated[str, Field(min_length=3, max_length=50, description="Nome do produto")]
+    preco: Annotated[float, Field(gt=0, le=10000, description="Preço do produto em reais")]
+    estoque: Annotated[int, Field(ge=0, description="Quantidade disponível em estoque")]
+
+class ProdutoResposta(BaseModel):
+    id: Annotated[int, Field(ge=0, description="ID do produto")]
+    nome: Annotated[str, Field(min_length=3, max_length=50, description="Nome do produto")]
+    preco: Annotated[float, Field(gt=0, le=10000, description="Preço do produto em reais")]
+    estoque: Annotated[int, Field(ge=0, description="Quantidade disponível em estoque")]
+
+class Login(BaseModel):
+    username: str
+    senha: str
+
+def criar_token(username):
+    return jwt.encode({'sub': username}, SECRET_KEY, algorithm='HS256')
+
+def verificar_token(token):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    except InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token inválido")
+
+def verificar_autenticacao(credentials = Depends(security)):
+    token = credentials.credentials
+    return verificar_token(token)
+
+@app.get('/')
+def inicio():
+    return {'message': 'API funcionando!'}
+
+@app.post('/login')
+def login(dados: Login):
+    usuario = buscar_usuario_username(dados.username)
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Usuário ou senha inválidos")
+    if not verificar_senha(dados.senha, usuario["senha"]):
+        raise HTTPException(status_code=401, detail="Usuário ou senha inválidos")
+    return criar_token(dados.username)
+
+@app.get('/produtos', response_model=list[ProdutoResposta])
+def produtos(nome=None):
+    if nome is None:
+        return listar_produtos()
+    else:
+        resultado = buscar_produtos_nome(nome)
+        if resultado:
+            return resultado
+    raise HTTPException(status_code=404, detail="Produto não encontrado")
+
+@app.get('/produtos/{id}', response_model=ProdutoResposta)
+def produto(id: int):
+    resultado = buscar_produto_id(id)
+    if resultado:
+        return resultado
+    raise HTTPException(status_code=404, detail="Produto não encontrado")
+
+@app.get('/sobre')
+def sobre(usuario = Depends(verificar_autenticacao)):
+    return {"nome": "API REST de Loja", "versao": "1.0"}
+
+@app.post('/produtos', response_model=ProdutoResposta)
+def criar_produto(produto: ProdutoCriacao, usuario = Depends(verificar_autenticacao)):
+    novo_produto = adicionar_produto(produto.nome, produto.preco, produto.estoque)
+    return buscar_produto_id(novo_produto)
+
+@app.put('/produtos/{id}', response_model=ProdutoResposta)
+def atualizar_produto(id:int, novo_produto: ProdutoCriacao, usuario = Depends(verificar_autenticacao)):
+    resultado = buscar_produto_id(id)
+    if resultado:
+        atualizar_produto_db(id, novo_produto.nome, novo_produto.preco, novo_produto.estoque)
+        return buscar_produto_id(id)
+    raise HTTPException(status_code=404, detail="Produto não encontrado")
+
+@app.delete('/produtos/{id}')
+def excluir_produto(id:int, usuario = Depends(verificar_autenticacao)):
+    resultado = buscar_produto_id(id)
+    if resultado:
+        excluir_produto_db(id)
+        return {'message': 'Produto excluído com sucesso!'}
+    return {'message': 'Produto não encontrado'}
+
+@app.get('/produtos/estoque/{quantidade}')
+def armazenamento(quantidade: int):
+    resultado = buscar_produtos_estoque(quantidade)
+    if resultado:
+        return resultado
+    return {'message': 'Nenhum produto encontrado com essa quantidade de estoque'}
